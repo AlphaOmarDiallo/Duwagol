ef46b4b73a2ebaa50e6588fc972dda5ec80e8947
Alpha <adiallo@exomind.fr>
2023-03-13 09:12:22 +0100

    project structure updated

--- app/src/main/java/com/alphaomardiallo/duwagol/common/ui/theme/Type.kt
+++ app/src/main/java/com/alphaomardiallo/duwagol/common/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.alphaomardiallo.duwagol.ui.theme
+package com.alphaomardiallo.duwagol.common.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
