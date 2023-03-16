2e4428d0ad3f798455f4e83a27c22a5e5306ace2
Alpha <adiallo@exomind.fr>
2023-03-17 00:46:13 +0100

    ws complete

---
+++ app/src/main/java/com/alphaomardiallo/duwagol/common/data/remote/prayerTimeCalendar/PrayerTimesCalendarWS.kt
@@ -0,0 +1,35 @@
+package com.alphaomardiallo.duwagol.common.data.remote.prayerTimeCalendar
+
+import com.alphaomardiallo.duwagol.common.domain.model.prayerTimesCalendar.ResponsePrayerTimeCalendar
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * Web service to fetch prayer time for a location
+ *
+ * [Link to the API](https://aladhan.com/prayer-times-api#GetCalendar)
+ */
+interface PrayerTimesCalendarWS {
+    @GET(URL)
+    suspend fun getPrayerTimesCalendar(
+        @Query(LATITUDE) lat: Double,
+        @Query(LONGITUDE) lng: Double,
+        @Query(MONTH) month: Int,
+        @Query(METHOD) method: Int,
+        @Query(SHAFAQ) shafaq: String,
+        @Query(SCHOOL) school: String,
+        @Query(MIDNHIGHT_MODE) midnightMode: Int,
+    ): Response<ResponsePrayerTimeCalendar>
+
+    companion object {
+        const val URL = "v1/calendar"
+        const val LATITUDE = "latitude"
+        const val LONGITUDE = "longitude"
+        const val MONTH = "month"
+        const val METHOD = "method"
+        const val SHAFAQ = "shafaq"
+        const val SCHOOL = "school"
+        const val MIDNHIGHT_MODE = "midnightMode"
+    }
+}
